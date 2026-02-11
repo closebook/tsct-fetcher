c0097845d22d8d8fb9e0c340b94851b60ce9b79f
closebook <siddharth24112004@gmail.com>
2026-02-11 16:14:43 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ async function startScraping() {
 
   const browser = await puppeteer.launch({
     headless: true,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
