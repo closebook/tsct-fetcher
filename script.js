55df874675d8e16336822138ec9dbef72bcfd728
closebook <siddharth24112004@gmail.com>
2026-02-11 14:46:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function applySearch() {
 }
 
 async function fetchUpdates() {
-  const res = await fetch("https://tsct-fetcher.onrender.com/api/data");
+  const res = await fetch("https://tsct-fetcher.onrender.com/");
   const json = await res.json();
 
   const oldLength = uniqueData.length;
@@ -107,4 +107,5 @@ document.getElementById("prevTop").onclick = goPrev;
 document.getElementById("searchBox").addEventListener("input", applySearch);
 
 fetchUpdates();
+
 
